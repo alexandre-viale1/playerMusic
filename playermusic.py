c1624dad423b248276d5c3da8dc4dc19f8eca6a6
alexandre-viale1 <115146018+alexandre-viale1@users.noreply.github.com>
2023-02-06 09:14:43 +0100

    projet playerMusic

---
+++ playermusic.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import pygame
+from tkinter import filedialog
+
+def bt_play():
+    pygame.mixer.music.play()
+
+def bt_pause():
+    pygame.mixer.music.pause()
+
+def bt_stop():
+    pygame.mixer.music.stop()
+
+def bt_volume_plus():
+    volume = pygame.mixer.music.get_volume()
+    if volume < 1.0:
+        pygame.mixer.music.set_volume(volume + 0.1)
+
+def bt_volume_moins():
+    volume = pygame.mixer.music.get_volume()
+    if volume > 0.0:
+        pygame.mixer.music.set_volume(volume - 0.1)
+
+def bt_loop():
+    pygame.mixer.music.play(-1)
+
+def bt_choix():
+    track = filedialog.askopenfilename(filetypes=[("Audio Files", "*.mp3;*.wav;*.ogg")])
+    pygame.mixer.music.load(track)
+
+fenetre = tk.Tk()
+fenetre.title("Audio Player")
+fenetre.geometry("190x150")
+
+bouton_play = tk.Button(fenetre, text="Play", command=bt_play)
+bouton_play.grid(row=0,column=1)
+bouton_pause = tk.Button(fenetre, text="Pause", command=bt_pause)
+bouton_pause.grid(row=1,column=1)
+bouton_stop = tk.Button(fenetre, text="Stop", command=bt_stop)
+bouton_stop.grid(row=2,column=1)
+bouton_volume_plus = tk.Button(fenetre, text="Volume +", command=bt_volume_plus)
+bouton_volume_plus.grid(row=1,column=0)
+bouton_volume_moins = tk.Button(fenetre, text="Volume -", command=bt_volume_moins)
+bouton_volume_moins.grid(row=2,column=0)
+bouton_loop = tk.Button(fenetre, text="Loop", command=bt_loop)
+bouton_loop.grid(row=1,column=2)
+bouton_choix_musique = tk.Button(fenetre, text="Choose Track", command=bt_choix)
+bouton_choix_musique.grid(row=2,column=2)
+
+
+
+pygame.mixer.init()
+fenetre.mainloop()
